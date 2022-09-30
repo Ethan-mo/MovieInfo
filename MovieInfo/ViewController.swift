b513b8122058a2feeb04b1690c3cf112747716ab
Ethan-mo <ethan.mo@goodmonit.com>
2022-09-30 13:30:53 +0900

    Initial Commit

---
+++ MovieInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieInfo
+//
+//  Created by 모상현 on 2022/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
